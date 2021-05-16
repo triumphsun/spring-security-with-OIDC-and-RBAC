088258de56fb1f20b155129b674300c8ff884eb5
Chia-Yang Sun <triumph.sun@gmail.com>
2021-05-16 19:31:48 +0800

    Enable global method security.

--- src/main/java/com/suntri/web/AccountController.java
+++ src/main/java/com/suntri/web/AccountController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.security.RolesAllowed;
 import java.text.SimpleDateFormat;
 import java.util.Optional;
 
@@ -20,6 +21,7 @@ public class AccountController {
 
     private SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss Z");
 
+    @RolesAllowed("ROLE_ADMIN")
     @RequestMapping(method = {RequestMethod.GET}, value = "/inspect/{email}")
     public ModelAndView inspect(@PathVariable("email") String email){
         ModelAndView mnv = new ModelAndView();
